91b378c972bb2fc9fc47a287ae89ea50737926ee
mborodaj <66844860+mborodaj@users.noreply.github.com>
2020-11-13 23:17:24 +0200

    code correction

--- js/task-7.js
+++ js/task-7.js
@@ -1,10 +1,8 @@
 const sliderRef = document.getElementById('font-size-control')
 let label = document.getElementById('text');
-console.log(label.innerText.fo);
 
 sliderRef.addEventListener('input', onInputChange);
 
 function onInputChange() {
-    console.log(sliderRef.value)
     label.style.fontSize = `${sliderRef.value}px`    
 }
